1d723716ecea0b65a7aaebeda567d18c1944444c
SevroAuBarca <daniel_conteras001@hotmail.com>
2022-12-12 21:47:20 -0600

    tienda

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const indexRouter = require("./src/routes/index");
-const usersRouter = require("./src/routes/users");
-const BooksAPI = require("./src/routes/books");
 const conn = require("./src/database");
+const TiendaAPI = require("./src/routes/tienda");
 
 const app = express();
 
@@ -17,10 +15,10 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "dist/frontend")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-BooksAPI(app);
+
+TiendaAPI(app);
+
 module.exports = app;
